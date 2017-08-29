16278351cb0f4f51afb4d1c1e8df26fa168ec0da
Mykola <30327771+myk-lop@users.noreply.github.com>
2017-08-29 17:30:54 +0300

    Initial commit

---
+++ psycology.care/js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: false,
+        items: 1,
+        mouseDrag: true,
+        touchDrag: true,
+        autoplay: false,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            }
+        }
+    })
+
+    ///    burger menu
+    $(function () {
+        $(".burger-menu").click(function () {
+            $(this).toggleClass("menu-on");
+            $('#mainMenu').toggleClass('active');
+            return false;
+        });
+
+        $(document).click(function (event) {
+            if ($(event.target).closest(".burger-menu").length) return;
+            if ($(event.target).closest("#mainMenu").length) return;
+            $('#mainMenu').removeClass('active');
+            $('.burger-menu').removeClass('menu-on');
+            event.stopPropagation();
+        });
+
+        $('.nav__list li ul li').click(function () {
+            $('#mainMenu').removeClass('active');
+            $('.burger-menu').removeClass('menu-on');
+            return false;
+        });
+
+    });
+});
+//    end of burger menu
+
+
